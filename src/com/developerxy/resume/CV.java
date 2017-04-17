fe43da1620ad3ce3467da021a6bed20e984a9cc0
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2017-04-17 22:53:09 +0100

    Create the 'CV' class & determine the necessary annotations to be created.

---
+++ src/com/developerxy/resume/CV.java
@@ -0,0 +1,44 @@
+package com.developerxy.resume;
+
+/**
+ * Created by Mohammed Aouf ZOUAG on 17/04/2017.
+ */
+/*
+
+@PersonalInfo(
+        @OwnerName("Mohammed Aouf ZOUAG"),
+        @OwnerDescription("Java/Android developer."),
+        @Email("mohammedaoufzouag@outlook.com"),
+        @Website("www.developerxy.io"),
+        @PhoneNumber("+212694363053")
+)
+@Experiences(
+        @Experience(title="Stage DUT", when="April 2016", description=""),
+        @Experience(title="Stage LP", when="April 2016", description=""),
+        @Experience(title="Stage MSc", when="April 2016", description="")
+)
+@Formations(
+        @Formation(when="", description=""),
+        @Formation(when="", description=""),
+        @Formation(when="", description=""),
+        @Formation(when="", description="")
+)
+@Projects(
+        @Project(name="", description=""),
+        @Project(name="", description=""),
+        @Project(name="", description="")
+)
+@Skills(
+        @Tech(name="", related={"", ""}),
+        @Tech(name="", related={"", ""}),
+        @Tech(name="", related={"", ""})
+)
+@Accounts(
+        @Account(icon="", nickname=""),
+        @Account(icon="", nickname=""),
+        @Account(icon="", nickname="")
+)
+*/
+
+public class CV {
+}
