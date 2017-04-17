9c3ec6be28c3e6b8d4cdde83c5e24043ff8f8465
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2017-04-17 23:03:42 +0100

    Create the PhoneNumber annotation.

---
+++ src/com/developerxy/resume/sections/personal/PhoneNumber.java
@@ -0,0 +1,15 @@
+package com.developerxy.resume.sections.personal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Mohammed Aouf ZOUAG on 17/04/2017.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface PhoneNumber {
+    String value();
+}
