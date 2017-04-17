bb7663b44ac606e1b6f797726c138ac0f15a09d4
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2017-04-17 23:11:41 +0100

    Create the Experience annotation.

---
+++ src/com/developerxy/resume/sections/exp/Experience.java
@@ -0,0 +1,17 @@
+package com.developerxy.resume.sections.exp;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Mohammed Aouf ZOUAG on 17/04/2017.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Experience {
+    String title();
+    String when();
+    String description();
+}
