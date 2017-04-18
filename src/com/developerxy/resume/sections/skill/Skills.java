64b626e99bc463e25495a03aec7b481236a817d2
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2017-04-18 04:41:32 +0100

    Create the Skills annotation.

---
+++ src/com/developerxy/resume/sections/skill/Skills.java
@@ -0,0 +1,15 @@
+package com.developerxy.resume.sections.skill;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Mohammed Aouf ZOUAG on 18/04/2017.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Skills {
+    Tech[] value();
+}
