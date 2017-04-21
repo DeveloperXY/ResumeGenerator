e07b0f38a1e082f99f20c70c18b89d03937f7150
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2017-04-21 22:52:31 +0100

    Remove duplicated class files.

--- src/com/developerxy/resume/util/OutputLocation.java
+++ src/com/developerxy/resume/util/OutputLocation.java
@@ -4,6 +4,8 @@ import java.lang.annotation.*;
 
 /**
  * Created by Mohammed Aouf ZOUAG on 20/04/2017.
+ *
+ * This annotation specifies a custom home directory, & the name of the generated HTML file.
  */
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
